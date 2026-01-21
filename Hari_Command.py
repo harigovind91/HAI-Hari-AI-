8930e596de48d0a3440128ac387abb26f78cf707
HAI (Hari AI) <86455355+harigovind91@users.noreply.github.com>
2026-01-22 00:35:18 +0530

    Delete Hari_Command.py

--- Hari_Command.py
+++
@@ -1,8 +0,0 @@
-print("------------------------------------")
-print("HAI (Hari AI) System: ONLINE")
-print("Master: Swami Harigovind Singh Chauhan")
-print("Status: Satellite Link Active")
-print("------------------------------------")
-
-
-
